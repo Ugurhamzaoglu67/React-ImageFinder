68dd728afd335052840d72a6f0e8b337e9f4cad8
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-05-11 21:37:10 +0300

    The App initialize

---
+++ src/components/My_app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+//_____________________ FUNCTION BASED______________
+
+const App = () => {
+    return(
+        <div>
+            <h1>Hello....</h1>
+        </div>
+    )
+}
+
+
+export default App // Let's export  it so I can call it from index.js
