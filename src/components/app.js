54e246cb495da2873eca7f854ba4efa14624901d
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-05-12 02:47:59 +0300

    images length disp.

--- src/components/app.js
+++ src/components/app.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react'
 import SearchBar from './searchBar/searchBar.js'
 import './styleapp.css'
-
-
-
+import axios from 'axios'
 
 
 class App extends Component {
 
-    onsearchClick(searchFor) {
-        console.log("app: "+searchFor)
+
+    state = {
+        images : []
     }
+
+    onsearchClick = async (searchFor) => {
+       const result = await axios.get("https://api.unsplash.com/photos/?client_id=G8KSg2m5YBTXtTKEUv7hGyPRVsSXlwwdUBCZSgeRHxU", {
+
+            params : {
+                query : searchFor
+            }
+
+        })
+        
+        console.log(result.data.length)
+       this.setState({
+           images : result.data
+       })
+    }
+
+
 
 
     render() {
         return (
-            <div className="appContainer" >
+
+          
+            <div className="appContainer">
                 <SearchBar onsearchClick={this.onsearchClick} />
+               
+                <div>
+                    {this.state.images.length}
+                </div>
             </div>
+      
         )
     }
 
 
 
-};
+}
 
 export default App;
